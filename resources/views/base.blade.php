172d0b9ab38977942cc64d314f334bd2d9a6fbb0
saad mouden <moudensaad03@gmail.com>
2024-05-21 17:06:34 +0200

    view changes

---
+++ resources/views/base.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Untree.co">
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
+
+    <meta name="description" content="" />
+    <meta name="keywords" content="bootstrap, bootstrap5" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('fonts/icomoon/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('fonts/flaticon/font/flaticon.css') }}">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="{{ asset('css/tiny-slider.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/aos.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/glightbox.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('css/flatpickr.min.css') }}">
+
+    <title>Shop smile</title>
+</head>
+
+<body>
+    
+    <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close">
+                <span class="icofont-close js-menu-toggle"></span>
+            </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+    </div>
+
+    <!-- Start head -->
+    <nav class="site-nav">
+        <div class="container">
+            <div class="menu-bg-wrap">
+                <div class="site-navigation">
+                    <div class="row g-0 align-items-center">
+                        <div class="col-2">
+                            <a href="index.html" class="logo m-0 float-start">Blogy<span
+                                    class="text-primary">.</span></a>
+                        </div>
+                        <div class="col-8 text-center">
+                            <form action="#" class="search-form d-inline-block d-lg-none">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="bi-search"></span>
+                            </form>
+
+                            <ul class="js-clone-nav d-none d-lg-inline-block text-start site-menu mx-auto">
+                                <li class="active"><a href="index.html">Home</a></li>
+                                <li class="has-children">
+                                    <a href="category.html">Pages</a>
+                                    <ul class="dropdown">
+                                        <li><a href="search-result.html">Search Result</a></li>
+                                        <li><a href="blog.html">Blog</a></li>
+                                        <li><a href="single.html">Blog Single</a></li>
+                                        <li><a href="category.html">Category</a></li>
+                                        <li><a href="about.html">About</a></li>
+                                        <li><a href="contact.html">Contact Us</a></li>
+                                        <li><a href="#">Menu One</a></li>
+                                        <li><a href="#">Menu Two</a></li>
+                                        <li class="has-children">
+                                            <a href="#">Dropdown</a>
+                                            <ul class="dropdown">
+                                                <li><a href="#">Sub Menu One</a></li>
+                                                <li><a href="#">Sub Menu Two</a></li>
+                                                <li><a href="#">Sub Menu Three</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a href="category.html">Culture</a></li>
+                                <li><a href="category.html">Business</a></li>
+                                <li><a href="category.html">Politics</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-2 text-end">
+                            <a href="#"
+                                class="burger ms-auto float-end site-menu-toggle js-menu-toggle d-inline-block d-lg-none light">
+                                <span></span>
+                            </a>
+                            <form action="#" class="search-form d-none d-lg-inline-block">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="bi-search"></span>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- end head -->
+
+    @yield('content')
+
+
+
+    <!-- Preloader -->
+    <div id="overlayer"></div>
+    <div class="loader">
+        <div class="spinner-border text-primary" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/tiny-slider.js') }}"></script>
+
+    <script src="{{ asset('js/flatpickr.min.js') }}"></script>
+
+    <script src="{{ asset('js/aos.js') }}"></script>
+    <script src="{{ asset('js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('js/navbar.js') }}"></script>
+    <script src="{{ asset('js/counter.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,70 @@
+@extends('base')
+
+@section('content')
+<!-- start products -->
+<section class="section">
+    <div class="container">
+
+        <div class="row mb-4">
+            <div class="col-sm-6">
+                <h2 class="posts-entry-title">Products</h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-4 mb-4">
+                <div class="post-entry-alt">
+                    <a href="single.html" class="img-link"><img src="images/img_7_horizontal.jpg" alt="Image"
+                            class="img-fluid"></a>
+                    <div class="excerpt">
+
+
+                        <h2><a href="single.html">Startup vs corporate: What job suits you best?</a></h2>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo sunt tempora dolor
+                            laudantium sed optio, explicabo ad deleniti impedit facilis fugit recusandae! Illo,
+                            aliquid, dicta beatae quia porro id est.</p>
+                        <p><a href="#" class="read-more">Continue Reading</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="post-entry-alt">
+                    <a href="single.html" class="img-link"><img src="images/img_6_horizontal.jpg" alt="Image"
+                            class="img-fluid"></a>
+                    <div class="excerpt">
+
+
+                        <h2><a href="single.html">Startup vs corporate: What job suits you best?</a></h2>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo sunt tempora dolor
+                            laudantium sed optio, explicabo ad deleniti impedit facilis fugit recusandae! Illo,
+                            aliquid, dicta beatae quia porro id est.</p>
+                        <p><a href="#" class="read-more">Continue Reading</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="post-entry-alt">
+                    <a href="single.html" class="img-link"><img src="images/img_5_horizontal.jpg" alt="Image"
+                            class="img-fluid"></a>
+                    <div class="excerpt">
+
+
+                        <h2><a href="single.html">Startup vs corporate: What job suits you best?</a></h2>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo sunt tempora dolor
+                            laudantium sed optio, explicabo ad deleniti impedit facilis fugit recusandae! Illo,
+                            aliquid, dicta beatae quia porro id est.</p>
+                        <p><a href="#" class="read-more">Continue Reading</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<!-- end products -->
+
+@endsection
